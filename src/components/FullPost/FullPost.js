88cf09f91c03b3a0214339f2f309ee2815278b8b
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-02-25 16:25:51 +0700

    Delete post, fixed bug show body

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -18,6 +18,14 @@ class FullPost extends Component {
             }
         }
     }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response)
+            })
+    }
+
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
         if (this.props.id) {
@@ -27,9 +35,9 @@ class FullPost extends Component {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
-                    <p>{this.state.loadedPost.content}</p>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
 
